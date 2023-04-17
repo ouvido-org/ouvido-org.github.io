9e3e9c30a4a1e703fea190adca280d8f6625c217
ouvido-org <128657250+ouvido-org@users.noreply.github.com>
2023-04-17 10:49:30 -0300

    commit

--- git-publicar.py
+++ git-publicar.py
@@ -1,6 +1,6 @@
 import subprocess as cmd
 cmd.run('git add -A', check=True, shell=True)
-cmd.run('git commit -m "message"', check=True, shell=True)
+cmd.run('git commit -m "commit"', check=True, shell=True)
 cmd.run("git push", check=True, shell=True)
 print("Success")
   
