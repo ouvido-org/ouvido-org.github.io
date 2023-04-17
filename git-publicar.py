9a4b3225dc86c1c74b7291bf9bf7b8e53beea5b4
ouvido-org <128657250+ouvido-org@users.noreply.github.com>
2023-04-17 10:25:59 -0300

    message

--- git-publicar.py
+++ git-publicar.py
@@ -1,5 +1,4 @@
 import subprocess as cmd
-cmd.run('cd /Users/shandi/Documents/GitHub/ouvido-org.github.io', check=True, shell=True)
 cmd.run('git add -A', check=True, shell=True)
 cmd.run('git commit -m "message"', check=True, shell=True)
 cmd.run("git push", check=True, shell=True)
